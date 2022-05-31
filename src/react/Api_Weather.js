c12e7f11ce8827d876e8f74ff17ae83d73a20645
Ruslan5787 <abdulazimov.03@mail.ru>
2022-05-31 17:19:44 +0500

    fix bags

--- src/react/Api_Weather.js
+++ src/react/Api_Weather.js
@@ -1,6 +1,6 @@
 const API = {
-  WETHER_URL: 'http://api.openweathermap.org/data/2.5/weather',
-  FORECAST_URL: 'http://api.openweathermap.org/data/2.5/forecast',
+  WETHER_URL: 'https://api.openweathermap.org/data/2.5/weather',
+  FORECAST_URL: 'https://api.openweathermap.org/data/2.5/forecast',
   KEY: '3dfd6d17e1726af829035cc9bd245628',
   DEGREES_CELSIUS: '&units=metric',
 };
